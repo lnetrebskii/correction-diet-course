ebe8867cf8b3f0d132281e959d36da5e068fb2dd
Leo <leon@entechsolutions.com>
2020-02-11 18:33:24 +0400

    #1 Add login/logout

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,48 @@
+import {Component, NgZone, OnInit} from '@angular/core';
+import * as firebase from "firebase";
+import * as firebaseui from "firebaseui";
+import {AngularFireAuth} from "@angular/fire/auth";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  ui: firebaseui.auth.AuthUI;
+
+  constructor(private router:Router,
+              private ngZone: NgZone) { }
+
+  ngOnInit() {
+
+    const uiConfig = {
+
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+
+      callbacks: {
+
+        signInSuccessWithAuthResult: this
+          .onLoginSuccessful
+          .bind(this)
+
+      }
+
+    };
+
+    this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    this.ui.start('#firebaseui-auth-container', uiConfig);
+
+  }
+
+  onLoginSuccessful(result) {
+    this.ngZone.run(() => this.router.navigateByUrl('/home'));
+  }
+
+}
